b88668926b005e70eee85fb584bda37fa3c68537
61251850 <LorAutumn@users.noreply.github.com>
2022-07-25 22:49:53 +0200

    add game logic

--- src/Component/GameGrid.tsx
+++ src/Component/GameGrid.tsx
@@ -1,5 +1,6 @@
 import { TextField } from '@mui/material'
 import { makeStyles } from '@mui/styles'
+import { useState } from 'react'
 
 const useStyles = makeStyles(() => {
     return {
@@ -15,11 +16,33 @@ const GameGrid = () => {
     const alphabet: Array<string> = [...Array(26)].map((_, i) =>
         String.fromCharCode(i + 65)
     )
+    const [gameInput, setGameInput] = useState<string[]>([])
+
+    const inputLogic = (
+        pushedKey: string,
+        gameInput: string[],
+        alphabet: string[]
+    ) => {
+        if (pushedKey === 'KeyY') {
+            pushedKey = 'KeyZ'
+        } else if (pushedKey === 'KeyZ') {
+            pushedKey = 'KeyY'
+        }
+
+        if (pushedKey.slice(3) === alphabet[gameInput.length]) {
+            setGameInput([...gameInput, pushedKey.slice(3)])
+        } else {
+            return
+        }
+        return
+    }
 
     return (
         <div className={classes.page}>
             <TextField
                 label={'A-Z'}
+                value={gameInput}
+                onKeyDown={e => inputLogic(e.code, gameInput, alphabet)}
                 inputProps={{
                     style: { textTransform: 'uppercase' },
                 }}></TextField>
